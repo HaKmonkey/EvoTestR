08f2f0904fb7b11516c864914b68ca03ffecaaef
Johnny Bravo <hakmonkey@gmail.com>
2018-03-04 17:12:02 -0700

    made changes to how plants were made

--- evotest.R
+++ evotest.R
@@ -9,23 +9,43 @@ evo.test <- function(timesteps = 50, terrain, herbivore.health = c(100), herbivo
   # plants are randomly assigned the three enzymes that digest them
   # the order of the enzymes gives the amount of health returned
   Enz <- c('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p')
-  plants <- list(c('p1'), c('p2'), c('p3'), c('p4'), c('p5'))
-  for(i in 1:5){
+
+  for(i in 1:3){
     temp <- sample(Enz, size = 3, replace = FALSE)
-    plants[[i]] <- plants[[i]] = temp
   }
+  p1 <- new.plant()
+
+  for(i in 1:3){
+    temp <- sample(Enz, size = 3, replace = FALSE)
+  }
+  p2 <- new.plant()
+
+  for(i in 1:3){
+    temp <- sample(Enz, size = 3, replace = FALSE)
+  }
+  p3 <- new.plant()
+
+  for(i in 1:3){
+    temp <- sample(Enz, size = 3, replace = FALSE)
+  }
+  p4 <- new.plant()
+
+  for(i in 1:3){
+    temp <- sample(Enz, size = 3, replace = FALSE)
+  }
+  p5 <- new.plant(temp[1], temp[2], temp[3])
 
   # quantiles for terrain height that constrain plant placement
-  prob.p1 <- quantile(terrain, probs = .22)
-  prob.p2 <- quantile(terrain, probs = seq(.2, .42))
-  prob.p3 <- quantile(terrain, probs = seq(.4, .62))
-  prob.p4 <- quantile(terrain, probs = seq(.6, .82))
-  prob.p5 <- quantile(terrain, probs = seq(.8, 1))
-  p1 <- c("", plants[[1]])
-  p2 <- c("", plants[[2]])
-  p3 <- c("", plants[[3]])
-  p4 <- c("", plants[[4]])
-  p5 <- c("", plants[[5]])
+  quant.p1 <- quantile(terrain, probs = .22)
+  quant.p2 <- quantile(terrain, probs = seq(.2, .42))
+  quant.p3 <- quantile(terrain, probs = seq(.4, .62))
+  quant.p4 <- quantile(terrain, probs = seq(.6, .82))
+  quant.p5 <- quantile(terrain, probs = seq(.8, 1))
+  choice.p1 <- c("", p1)
+  choice.p2 <- c("", p2)
+  choice.p3 <- c("", p3)
+  choice.p4 <- c("", p4)
+  choice.p5 <- c("", p5)
 
   # creating herbivore array
   herbivore.generation <- array(data = 0, dim = c(nrow(terrain), ncol(terrain), (timesteps + 1)))
@@ -41,16 +61,16 @@ evo.test <- function(timesteps = 50, terrain, herbivore.health = c(100), herbivo
       }else{
         # make sure plants are not placed outside of their quantile for height
         # setting probabilites now
-        if(terrain[r,c] <= prob.p1){
-          plant.generation[r, c, 1] <- sample(p1, size = 1, replace = FALSE, prob = .5)
-        }else if(terrain[r,c] <= prob.p2){
-          plant.generation[r, c, 1] <- sample(p2, size = 1, replace = FALSE, prob = .5)
-        }else if(terrain[r,c] <= prob.p3){
-          plant.generation[r, c, 1] <- sample(p3, size = 1, replace = FALSE, prob = .5)
-        }else if(terrain[r,c] <= prob.p4){
-          plant.generation[r, c, 1] <- sample(p4, size = 1, replace = FALSE, prob = .5)
-        }else if(terrain[r,c] <= prob.p5)
-          plant.generation[r, c, 1] <- sample(p5, size = 1, replace = FALSE, prob = .5)
+        if(terrain[r,c] <= quant.p1){
+          plant.generation[r, c, 1] <- sample(choice.p1, size = 1, replace = FALSE, prob = .5)
+        }else if(terrain[r,c] <= quant.p2){
+          plant.generation[r, c, 1] <- sample(choice.p2, size = 1, replace = FALSE, prob = .5)
+        }else if(terrain[r,c] <= quant.p3){
+          plant.generation[r, c, 1] <- sample(choice.p3, size = 1, replace = FALSE, prob = .5)
+        }else if(terrain[r,c] <= quant.p4){
+          plant.generation[r, c, 1] <- sample(choice.p4, size = 1, replace = FALSE, prob = .5)
+        }else if(terrain[r,c] <= quant.p5)
+          plant.generation[r, c, 1] <- sample(choice.p5, size = 1, replace = FALSE, prob = .5)
         herbivore.generation[r, c, 1] <- sample(herbivores, size = 1, replace = FALSE, prob = p.herb)
       }
     }
