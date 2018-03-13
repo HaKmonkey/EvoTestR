f981863ee8b857163280b9e890c96a4db7f5b776
Johnny Bravo <hakmonkey@gmail.com>
2018-03-13 11:08:24 -0600

    again, fixed to.dna()

--- evo.R
+++ evo.R
@@ -13,17 +13,16 @@ to.enz <- function(x){
   return(Enz[as.character(temp)])
 }
 
-to.dna <- function(x, possbible.enz){
-  Enz <- list('a' = 0, 'b' = 1, 'c' = 2, 'd' = 3, 'e' = 4, 'f' = 5, 'g' = 6, 'h' = 7, 'i' = 8, 'j' = 9, 'k' = 10, 'l' = 11, 'm' = 12, 'n' = 13, 'o' = 14, 'p' = 15)
+to.dna <- function(to.use){
+  bases <- c()
   DNA <- list('A', 'T', 'G', 'C')
   dna.list <- as.matrix(expand.grid(DNA, DNA, DNA, DNA, DNA))
   for(i in 1:nrow(dna.list)){
-    temp <- 1
-    # how to exit after the bases have been selected
-    if(temp == 5){
+    temp <- 0
+    if(temp == 3){
       return bases
     }
-    if(as.character(to.enz(dna.list[1,])) %in% possible.enz){
+    if(as.character(to.enz(dna.list[1,])) %in% to.use){
       bases <- c(bases, dna.list[1, ])
       temp <- temp + 1
     }
